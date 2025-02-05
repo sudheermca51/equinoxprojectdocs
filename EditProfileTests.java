bcef9b618d7dca8ff1fc236b49713faeb0a2d2e1
sudheermca51 <37737996+sudheermca51@users.noreply.github.com>
2025-02-05 08:48:07 +0530

    Add files via upload

---
+++ EditProfileTests.java
@@ -0,0 +1,28 @@
+package org.iitwf.selenium.mmpequinox;
+
+public class EditProfileTests {
+	/**
+	 * 1. update the firstname by passing randomtext value.
+	 * 	  expected and actual 
+	 *    compare expected vs actual
+	 *  
+	 * 2. LAstName
+	 * 3. SSN
+	 * 4. Address
+	 * 
+	 * SoftAssert 
+	 * 
+	 * FluentWait
+	 * Breakpoint by modifying the logic
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+
+}
